7a3b2731115ee294d26438bb0bf79ce313d9ba4d
Daria <dashuta.magdeeva@mail.ru>
2024-05-08 10:45:58 +0300

    xmlWrite

--- WeatherService.cpp
+++ WeatherService.cpp
@@ -1,6 +1,7 @@
 ﻿#include <string>
 #include <iostream>
 #include "Weather.h"
+#include "XmlService.h"
 using namespace std;
 
 int main()
@@ -8,5 +9,9 @@ int main()
     setlocale(LC_ALL, "rus");
     Weather weather("Киров", 49.6601, 58.5966, 5.69, "дождь", 4.27, 100);
     weather.print();
+
+    XmlService xs;
+    Weather w = xs.getWeather("weather.xml");
+    w.print();
     return 0;
 }
